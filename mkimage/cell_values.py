b93a0bb7fc4355d18f6b9515ae393968ed82bcb1
Mateusz Kozak <mateusz.kozak@unige.ch>
2020-05-28 10:01:43 +0200

    Add a return statement to cell_values
    cell_values.py can save various measurements as files; now it also
    always returns a dictionary in filename:values format.

--- mkimage/cell_values.py
+++ mkimage/cell_values.py
@@ -1,17 +1,23 @@
-# %%
-from im_tools import *
-
 # import modules for handling files
+import csv
 from pathlib import Path
 from sys import argv
-import csv
+
+# import third-party packages
+import numpy as np
+import tifffile as tiff
+from skimage.exposure import rescale_intensity
+
+# import utility functions
+from .utility import mask_cell
 
 # %%
 def batch_mask(path, pattern="*GFP*",
-               rescale=False, return_dict=False, save_arrays=False, save_summary=False):
+               rescale=False, save_arrays=False, save_summary=False):
     """
     This function reads all images with a keyword ('GFP' by default) and applies a 3D masking procedure.
-    You need to explicitly tell the function whether it should return a dictionary with all pixel values or save each array as a .txt file.
+    If save_arrays, each array will be saved as a .txt file.
+    Summary writes a .csv file with summary statistics per cell (mean, median, sd).
     Rescale option forces each array into [0,1] range.
     """
     # path handling through Pathlib: make output folder within current path
@@ -29,7 +35,7 @@ def batch_mask(path, pattern="*GFP*",
         im = im[mask_cell(im)]  # mask and select values
         # optional rescaling
         if rescale:
-            im = rescale_to_float(im)
+            im = rescale_intensity(im, out_range='float')
         # add dictionary entry with name (no extension) and pixel values
         pixels[i.name.replace('.tif', '')] = im
         # add image name to output folder path
@@ -55,11 +61,10 @@ def batch_mask(path, pattern="*GFP*",
             writer = csv.writer(f, dialect='excel')
             writer.writerow(['cell', 'mean', 'median', 'sd'])
             for key, value in pixels.items():
-                writer.writerow([key, round(np.mean(value),
-                                 np.median(value), np.std(value)])
+                writer.writerow([key, round(np.mean(value), 3),
+                                 np.median(value), round(np.std(value), 3)])
     # output: return dictionary of masked pixels
-    if return_dict:
-        return(pixels)
+    return(pixels)
 
 
 # get the path from command line and run counting function
